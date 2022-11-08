2cfa7caf481b67d4eceebc51007923cf1e557451
alishervoitov <114944816+alishervoitov@users.noreply.github.com>
2022-11-08 10:29:20 +0500

    cinema

---
+++ django_movie/movies/admin.py
@@ -0,0 +1,67 @@
+from django.contrib import admin
+
+from .models import Category, Actor, Genre, Movie, MovieShots, Rating, RatingStar, Reviews
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'url')
+    list_display_links = ('name',)
+
+class ReviewInline(admin.TabularInline):
+    model = Reviews
+    extra = 1
+    readonly_fields = ('name', 'email')
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url', 'draft')
+    list_filter = ('category', 'year')
+    search_fields = ('title', 'category__name')
+    inlines = [ReviewInline]
+    save_on_top = True
+    save_as = True
+    list_editable = ('draft',)
+    fieldsets = (
+        (None, {
+            'fields': (('title', 'tagline'), )
+        }),
+        (None, {
+            'fields': ('description', 'poster')
+        }),
+        (None, {
+            'fields': (('year', 'world_premiere', 'country'),)
+        }),
+        (None, {
+            'fields': (('actors', 'directors', 'genres', 'category'),)
+        }),
+        ('Actors', {
+            'classes': ('collapse',),
+            'fields': (('budget', 'fees_in_usa', 'fess_in_world'),)
+        }),
+        ('Options', {
+            'fields': (('url', 'draft'),)
+        }),
+    )
+
+@admin.register(Reviews)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'parent', 'movie', 'id')
+    readonly_fields = ('name', 'email')
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'url')
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age')
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('star', 'ip')
+
+@admin.register(MovieShots)
+class MovieShotsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'movie')
+
+admin.site.register(RatingStar)
